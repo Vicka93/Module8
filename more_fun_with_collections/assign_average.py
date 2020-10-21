24dd427cf574a49f1d0e489a0608cac9a6f14b59
Vicka93 <57420845+Vicka93@users.noreply.github.com>
2020-10-20 23:38:04 -0500

    assign_average.py switch_average is implemented , all tests passed

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,12 +1,22 @@
 """
 Author:Viktoria Denys
 Program:assign_average.py
+function accepts entry and return value from dictionary. if non-key passed statement from
+my_dict.get() function will be returned
 """
 
-
+# function accepts entry and return a value from dictionary
 def switch_average(entry):
-    pass
+    # created a dictionary for grades and statements
+    my_dict = {'A': "You entered an A",
+               'B': "You entered an B",
+               'C': "You entered an C",
+               'D': "You entered an D",
+               'F': "You entered an F"}
+    # if a non-key is passed , it will return a statement below ""{entry} is not a valid Grade".format(entry.upper()"
+    result = my_dict.get(entry.upper(), "{} is not a valid Grade".format(entry.upper()))
+    return result
 
 
 if __name__ == '__main__':
-    print()
+    print(switch_average('g'))
--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -3,23 +3,33 @@ from more_fun_with_collections import assign_average
 
 
 class MyTestCase(unittest.TestCase):
+    # test function pass for key value of 'A'
     def test_A(self):
         self.assertEqual("You entered an A", assign_average.switch_average('A'))
 
+    # test function pass for key value of 'a', upper() added to switch_average
+    def test_a(self):
+        self.assertEqual("You entered an A", assign_average.switch_average('a'))
+
+    # test function pass for key value of 'B'
     def test_B(self):
         self.assertEqual("You entered an B", assign_average.switch_average('B'))
 
+    # test function pass for key value of 'C'
     def test_C(self):
         self.assertEqual("You entered an C", assign_average.switch_average('C'))
 
+    # test function pass for key value of 'D'
     def test_D(self):
         self.assertEqual("You entered an D", assign_average.switch_average('D'))
 
+    # test function pass for key value of 'F'
     def test_F(self):
         self.assertEqual("You entered an F", assign_average.switch_average('F'))
 
+    # test function pass for non_key value
     def test_non_key(self):
-        self.assertEqual("This is not a valid Grade", assign_average.switch_average('R'))
+        self.assertEqual("R is not a valid Grade", assign_average.switch_average('R'))
 
 
 if __name__ == '__main__':
